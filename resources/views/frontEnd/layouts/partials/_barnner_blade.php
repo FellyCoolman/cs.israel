fd14124bebf45d284757f32c7abaf34f07b9a77b
felly ilunga <fellyunikin7@gmail.com>
2020-10-01 01:34:59 +0200

    first part of the incormporation onf the front bootstrap template

---
+++ resources/views/frontEnd/layouts/partials/_barnner_blade.php
@@ -0,0 +1,56 @@
+
+	<!--================ Start Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="banner_content">
+							<h2>
+								We Rank the Best <br>
+								Courses on the Web
+							</h2>
+							<p>
+								In the history of modern astronomy, there is probably no one greater leap forward than the building and launch
+								of
+								the space telescope known as the Hubble.
+							</p>
+							<div class="search_course_wrap">
+								<form action="" class="form_box d-flex justify-content-between w-100">
+									<input type="text" placeholder="Search Courses" class="form-control" name="username" onfocus="this.placeholder = ''"
+									 onblur="this.placeholder = 'Search Courses'">
+									<button type="submit" class="btn search_course_btn">Search</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Home Banner Area =================-->
+
+
+<!--================ Banner Area =================-->
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="banner_content text-center">
+                        <h2>About Us</h2>
+                        <p>In the history of modern astronomy, there is probably no one greater leap forward than
+                            the
+                            building and launch of the space telescope known as the Hubble.</p>
+                        <div class="page_link">
+                            <a href="index.html">Home</a>
+                            <a href="about-us.html">About</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
